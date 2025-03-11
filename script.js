836566bf06aa2f4da5d2857e44729bf704d34243
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-03-11 23:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function convertToRoman(num){
-	if (num <= 0 || num > 100000 ) return "Invalid input";
-
-	const romanNumerals = [
-		["M", 1000], ["CM", 900], ["D", 500], ["CD", 400],
-		["C", 100], ["XC", 90], ["L", 50], ["XL", 40],
-		["X", 10], ["IX", 9], ["V", 5], ["IV", 4], ["I",1]
-	];
-	let result = "";
-	for (let [symbol, value] of romanNumerals) {
-		while (num >= value) {
-			result += symbol;
-			num -= value;
-			
-		}
-	}
-	return result;
+function convertToRoman(num) {
+    if (num <= 0 || num > 100000) return "Invalid input";
+    
+    const romanNumerals = [
+        ["M", 1000], ["CM", 900], ["D", 500], ["CD", 400],
+        ["C", 100], ["XC", 90], ["L", 50], ["XL", 40],
+        ["X", 10], ["IX", 9], ["V", 5], ["IV", 4], ["I", 1]
+    ];
+    
+    let result = "";
+    for (let [symbol, value] of romanNumerals) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+    return result;
 }
-console.log(convertToRoman(14));
+
+// Example usage
+console.log(convertToRoman(14));  // Output: XIV
+console.log(convertToRoman(798)); // Output: DCCXCVIII
